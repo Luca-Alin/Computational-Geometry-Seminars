76a665317692fc5f81ae0e285641284ff8f0c53a
computer88 <alinluca20021@gmail.com>
2023-05-08 01:40:40 +0300

    Update

--- src/main/java/gd/seminar4/S4P1.java
+++ src/main/java/gd/seminar4/S4P1.java
@@ -1,18 +1,188 @@
 package gd.seminar4;
 
 import gd.frame.DefaultPanel;
+import gd.frame.Dimensions;
 
+import javax.swing.Timer;
 import java.awt.*;
+import java.util.*;
+import java.util.List;
 
 public class S4P1 extends DefaultPanel {
 
+    int n;
+    List<Point> points;
+    Random random = new Random();
+    javax.swing.Timer timer;
+    int count1 = 0, count2 = 0, count3 = 0;
+    List<List<Point>> upperHull = new ArrayList<>();
+    List<List<Point>> aux = new ArrayList<>();
+    List<List<Point>> lowerHull = new ArrayList<>();
+
     public S4P1() {
         super();
+
+        n = 10;
+        points = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = random.nextInt(40, Dimensions.WIDTH - 40);
+            int y = random.nextInt(40, Dimensions.HEIGHT - 40);
+            points.add(new Point(x, y));
+        }
+
+        ConvexHull convexHull = new ConvexHull();
+        List<Point> ans = convexHull.convexHull(points);
+
+
+        timer = new Timer(200, actionEvent -> {
+            if (count1 < points.size())
+                count1++;
+            else if (count2 < upperHull.size() - 1)
+                count2++;
+            else
+                timer.stop();
+            this.repaint();
+        });
+        timer.start();
+
+
     }
+
+
 
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
+        Graphics2D g = (Graphics2D) graphics;
+
+        g.setStroke(new BasicStroke(4));
+        g.setColor(Color.BLACK);
+        for (int i = 0; i < count1; i++) {
+            g.drawOval(points.get(i).x, points.get(i).y, 6, 6);
+        }
+
+        for (int i = 0; i <= count2; i++) {
+            List<Point> awt = upperHull.get(i);
+            for (int j = 0; j < awt.size() - 1; j++) {
+                Point p1 = awt.get(j);
+                Point p2 = awt.get(j + 1);
+                g.drawLine(p1.x, p1.y, p2.x, p2.y);
+            }
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class ConvexHull {
+
+
+        // Cross product of two vectors OA and OB
+        // returns positive for counter clockwise
+        // turn and negative for clockwise turn
+        static long crossProduct(Point O, Point A, Point B) {
+            return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x);
+        }
+
+        // Returns a list of points on the convex hull
+        // in counter-clockwise order
+        List<Point> convexHull(List<Point> points) {
+            int n = points.size(), k = 0, fresco = 0;
+
+            if (n <= 3)
+                return points;
+
+            List<Point> hullPoints = new ArrayList<>(2 * n);
+
+            // Sort points lexicographically
+            Collections.sort(points, new Comparator<Point>() {
+                @Override
+                public int compare(Point p1, Point p2) {
+                    if (p1.x == p2.x)
+                        return p1.y - p2.y;
+                    return p1.x - p2.x;
+                }
+            });
+
+            // Build lower hull
+            for (int i = 0; i < n; ++i) {
+                // If the point at K-1 position is not a part
+                // of hull as vector from ans[k-2] to ans[k-1]
+                // and ans[k-2] to A[i] has a clockwise turn
+                while (k >= 2
+                        && crossProduct(hullPoints.get(k - 2),
+                        hullPoints.get(k - 1), points.get(i))
+                        <= 0) {
+                    hullPoints.remove(--k);
+                }
+                hullPoints.add(points.get(i));
+                List<Point> list = new ArrayList<>(hullPoints);
+                lowerHull.add(list);
+                k++;
+            }
+
+            // Build upper hull
+            for (int i = n - 2, t = k; i >= 0; --i) {
+
+                // If the point at K-1 position is not a part
+                // of hull as vector from ans[k-2] to ans[k-1]
+                // and ans[k-2] to A[i] has a clockwise turn
+                while (k > t && crossProduct(hullPoints.get(k - 2), hullPoints.get(k - 1), points.get(i)) <= 0) {
+                    hullPoints.remove(--k);
+                }
+                hullPoints.add(points.get(i));
+                List<Point> list = new ArrayList<>(hullPoints);
+                upperHull.add(list);
+                k++;
+            }
+
+            for (int i = 0; i < upperHull.size(); i++) {
+                List<Point> list = upperHull.get(i);
+                int kappa = lowerHull.get(lowerHull.size() - 1).size();
+                int fr = upperHull.get(i).size();
+//                System.out.println(list.subList(kappa, fr));
+                aux.add(new ArrayList<>(upperHull.get(i).subList(kappa, fr)));
+//                System.out.println(aux.size());
+            }
+//            System.out.println("--------------------------------------------------------------------------------------------");
+            for (int i = 0; i < lowerHull.size(); i++) {
+                List<Point> list = lowerHull.get(i);
+//                System.out.println(list);
+            }
+            // Resize the array to desired size
+
+            return hullPoints;
+        }
 
 
     }
--- src/main/java/gd/seminar5/S5P1.java
+++ src/main/java/gd/seminar5/S5P1.java
@@ -1,19 +1,122 @@
 package gd.seminar5;
 
 import gd.frame.DefaultPanel;
+import gd.frame.Dimensions;
 
+import java.util.*;
 import java.awt.*;
+import java.util.List;
 
 public class S5P1 extends DefaultPanel {
 
+    int n;
+    Point[] points;
+    Random random = new Random();
+    List<List<Point>> convexHullList = new ArrayList<>();
+    int count1 = 0;
+    int count2 = 0;
+    int count3 = 0;
+    javax.swing.Timer timer;
     public S5P1() {
         super();
+
+        n = 20;
+        points = new Point[n];
+        for (int i = 0; i < points.length; i++) {
+            int x = random.nextInt(40, Dimensions.WIDTH - 40);
+            int y = random.nextInt(40, Dimensions.HEIGHT - 40);
+            points[i] = new Point(x, y);
+        }
+
+        new ConvexHull().convexHull(points, points.length);
+
+
+        timer = new javax.swing.Timer(100, actionEvent -> {
+            if (count1 < points.length)
+                count1++;
+            else if (count2 < convexHullList.size() - 1)
+                count2++;
+            else if (count3 == 0)
+                count3++;
+            else
+                timer.stop();
+            this.repaint();
+        });
+        timer.start();
     }
 
     @Override
     public void paint(Graphics graphics) {
         super.paint(graphics);
+        Graphics2D g = (Graphics2D) graphics;
+
+        g.setStroke(new BasicStroke(4));
+        g.setColor(Color.BLACK);
+
+        for (int i = 0; i < count1; i++) {
+            g.drawOval(points[i].x, points[i].y, 6, 6);
+        }
+
+        g.setColor(Color.RED);
+        int j = count2;
+        List<Point> awt = convexHullList.get(j);
+        for (int i = 0; i < awt.size() - 1; i++) {
+            Point p1 = awt.get(i);
+            Point p2 = awt.get(i + 1);
+            g.drawLine(p1.x, p1.y, p2.x, p2.y);
+        }
+
+        if (count3 > 0) {
+            awt = convexHullList.get(convexHullList.size() - 1);
+            Point p1 = awt.get(0);
+            Point p2 = awt.get(awt.size() - 1);
+            g.drawLine(p1.x, p1.y, p2.x, p2.y);
+        }
+
+    }
 
 
+    class ConvexHull
+    {
+        int orientation(Point p, Point q, Point r)
+        {
+            int val = (q.y - p.y) * (r.x - q.x) -
+                    (q.x - p.x) * (r.y - q.y);
+
+            if (val == 0) return 0;
+
+            return (val > 0)? 1: 2;
+        }
+
+        void convexHull(Point[] points, int n)
+        {
+            if (n < 3) return;
+
+            java.util.List<Point> hull = new ArrayList<>();
+
+            int l = 0;
+            for (int i = 1; i < n; i++)
+                if (points[i].x < points[l].x)
+                    l = i;
+
+            int p = l, q;
+            do
+            {
+                hull.add(points[p]);
+                convexHullList.add(new ArrayList<>(hull));
+
+                q = (p + 1) % n;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (orientation(points[p], points[i], points[q]) == 2)
+                        q = i;
+                }
+
+
+                p = q;
+
+            } while (p != l);
+        }
     }
 }
